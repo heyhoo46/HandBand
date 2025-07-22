34a9e13987ba9a606956af71c5c3533eaa9f23db
immune <dbswo2638@gmail.com>
2025-07-22 10:07:12 +0900

    Test 끝나고 Bit로 effect 신호 stm에서 PC로 Uart 내보내기

---
+++ ProcessControlUnit/250720_Final_project/Core/ap/Listener/Listener.c
@@ -0,0 +1,17 @@
+/*
+ * Listener.c
+ *
+ *  Created on: Jul 16, 2025
+ *      Author: kccistc
+ */
+
+
+void Listener_Init()
+{
+
+}
+
+void Listener_Excute()
+{
+
+}
---
+++ ProcessControlUnit/250720_Final_project/Core/ap/Model/LC_Model.h
@@ -0,0 +1,28 @@
+/*
+ * LC_Model.h
+ *
+ *  Created on: Jul 17, 2025
+ *      Author: kccistc
+ */
+
+#ifndef MODEL_INC_LC_MODEL_H_
+#define MODEL_INC_LC_MODEL_H_
+
+#include <stdint.h>
+#include <cmsis_os.h>
+#include <vector.h>
+#include <string.h>
+
+#define DATANUM 10
+
+typedef struct {
+	Point pointArr[DATANUM];
+}user_t;
+
+extern osMailQId pointDataMbox;
+
+void LC_Model_Init();
+void LC_Model_PushUser(user_t* data);
+void LC_Model_PopUser(user_t* data);
+
+#endif /* MODEL_INC_LC_MODEL_H_ */
